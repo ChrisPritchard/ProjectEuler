a87da99f9757b8b7b06c8ee9c25062cac63449f2
Chris <chris@grislygrotto.nz>
2025-04-12 08:35:01 +1200

    finished problem 1

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("hello world")
+	problem_001()
+}
+
+func problem_001() {
+	sum := 0
+	for i := 3; i < 1000; i += 3 {
+		sum += i
+	}
+	for j := 5; j < 1000; j += 5 {
+		if j%3 != 0 {
+			sum += j
+		}
+	}
+	println("problem 001: ", sum)
 }
