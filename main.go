9dccb125eee09fbd0b3b78fd48b539e9919bb33a
Chris <chris@grislygrotto.nz>
2025-04-20 22:29:47 +1200

    solved 31

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	Problems_001_010()
 	Problems_011_020()
 	Problems_021_030()
+
+	problem_031()
+}
+
+func problem_031() {
+
+	dp := make([]int, 200+1)
+	dp[0] = 1
+	for _, c := range []int{1, 2, 5, 10, 20, 50, 100, 200} {
+		for i := c; i < len(dp); i++ {
+			dp[i] += dp[i-c]
+		}
+	}
+
+	fmt.Println("problem 031:", dp[200])
 }
