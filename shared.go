08062051c5d5d6f82ebe3762385f55fda8c49519
Chris <chris@grislygrotto.nz>
2025-04-22 22:16:11 +1200

    created a shared library, working on permutations

---
+++ shared.go
@@ -0,0 +1,68 @@
+package main
+
+import "slices"
+
+// calculate all primes in the range 1 to max using the prime sieve method
+func prime_sieve(max int) []int {
+	res := []int{1}
+	sieve := make([]bool, max)
+
+	for i := 2; i < len(sieve); i++ {
+		if sieve[i] {
+			continue
+		}
+
+		res = append(res, i)
+		for j := i + i; j < len(sieve); j += i {
+			sieve[j] = true
+		}
+	}
+
+	return res
+}
+
+// simple integer power function
+func pown(base int, power uint) int {
+	if power == 0 {
+		return 1
+	}
+	n := base
+	for power > 1 {
+		n *= base
+		power--
+	}
+	return n
+}
+
+// separates a number into its digits, e.g. 123 becomes 1,2,3
+func value_to_digits(value int) []int {
+	res := []int{}
+
+	for value != 0 {
+		res = append(res, value%10)
+		value /= 10
+	}
+
+	slices.Reverse(res)
+	return res
+}
+
+// converts digits into a number, e.g. 1,2,3 becomes 123
+func digits_to_value(digits []int) int {
+	sum := 0
+	slices.Reverse(digits)
+	for i, v := range digits {
+		sum += pown(10, uint(i)) * v
+	}
+	return sum
+}
+
+// returns all possible combinations of the input array
+func permute(values []int) [][]int {
+
+	var expander func([]int, []int) [][]int
+	expander = func(acc, rem []int) [][]int {
+
+	}
+
+}
