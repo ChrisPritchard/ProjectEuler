d9b6cd945d3d448fae5f135bd318a652811f6282
= <=>
2025-05-02 20:15:09 +1200

    reorganised

--- problems_041_050.go
+++ problems_041_050.go
@@ -16,6 +16,7 @@ func Problems_041_050() {
 	problem_047()
 	problem_048()
 	problem_049()
+	problem_050()
 }
 
 func problem_041() {
@@ -259,4 +260,35 @@ func problem_049() {
 			}
 		}
 	}
+}
+
+func problem_050() {
+	primes := prime_sieve(1_000_000)
+	prime_set := NewSet(primes...)
+
+	max := make(map[int]int)
+	for i := range len(primes) - 1 {
+		n := primes[i]
+		for j := i + 1; j < len(primes); j++ {
+			n += primes[j]
+			if prime_set.Contains(n) {
+				if m, exists := max[n]; !exists || m < j-i {
+					max[n] = j - i
+				}
+			} else if n > primes[len(primes)-1] {
+				break
+			}
+		}
+	}
+
+	mk := 0
+	mv := 0
+	for k, v := range max {
+		if v > mv {
+			mv = v
+			mk = k
+		}
+	}
+
+	fmt.Println("problem 050:", mk)
 }
