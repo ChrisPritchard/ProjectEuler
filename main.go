9b0a019cf383208fb644e0a288bfbe5cae1e91e5
Chris <chris@grislygrotto.nz>
2025-05-19 19:21:05 +1200

    solved problem 51

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"slices"
 )
 
 func main() {
@@ -18,17 +20,68 @@ func main() {
 }
 
 func problem_051() {
-	extract_key := func(digits []int, positions []int) int {
-		transformer := func(i int) int {
-			if i < len(digits) {
-				return digits[i]
-			} else {
-				return 0
-			}
-		}
-		just_positions := transform(positions, transformer)
-		return digits_to_value(just_positions)
-	}
+	// get a million primes
+	// 8 primes could start at 0, 1 or 2 (3,4,5,6,7,8,9)
+	// so go through primes, discarding any without the first three
+	// when the first three are found, calculate positions and calculate candidates
 
 	primes := prime_sieve(1_000_000)
+	prime_set := NewSet(primes...)
+
+	indexes := func(digits []int, looked_for int) []int {
+		res := []int{}
+		for i := range digits {
+			if digits[i] == looked_for {
+				res = append(res, i)
+			}
+		}
+		return res
+	}
+
+	setter := func(digits []int, mask []int, replacement int) int {
+		for i := range digits {
+			if slices.Contains(mask, i) {
+				digits[i] = replacement
+			}
+		}
+
+		return digits_to_value(digits)
+	}
+
+	test_mask := func(digits []int, mask []int, start int) bool {
+		count := 1
+		for j := start; j <= 9; j++ {
+			candidate := setter(digits, mask, j)
+			if prime_set.Contains(candidate) {
+				count++
+			}
+		}
+		return count >= 8
+	}
+
+	for i := range primes {
+		prime_digits := value_to_digits(primes[i])
+		pos_0 := indexes(prime_digits, 0)
+		if len(pos_0) > 0 {
+			if test_mask(prime_digits, pos_0, 1) {
+				fmt.Println("problem 051:", primes[i])
+				return
+			}
+		}
+		pos_1 := indexes(prime_digits, 1)
+		if len(pos_1) > 0 {
+			if test_mask(prime_digits, pos_1, 2) {
+				fmt.Println("problem 051:", primes[i])
+				return
+			}
+		}
+		pos_2 := indexes(prime_digits, 2)
+		if len(pos_2) > 0 {
+			if test_mask(prime_digits, pos_2, 3) {
+				fmt.Println("problem 051:", primes[i])
+				return
+			}
+		}
+	}
+
 }
